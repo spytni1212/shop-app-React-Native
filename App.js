9a8e594ea3ec357001216b93616d9ee7a868e9fd
spytni1212 <avdeenkoartem1212@gmail.com>
2021-05-16 00:43:17 +0300

    feat: the products overview screen and setting up a navigator

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+import productsReducer from './store/reducers/products'
+import ShopNavigator from './navigation/ShopNavigator'
+
+const rootReducer = combineReducers({
+	products: productsReducer,
+})
+
+const store = createStore(rootReducer)
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Text>New App</Text>
-			<StatusBar style='auto' />
-		</View>
+		<Provider store={store}>
+			<ShopNavigator />
+		</Provider>
 	)
 }
 
---
+++ store/reducers/products.js
@@ -0,0 +1,12 @@
+import PRODUCTS from '../../data/dummy-data'
+
+const initialState = {
+	availableProducts: PRODUCTS,
+	userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1'),
+}
+
+const productsReducer = (state = initialState, action) => {
+	return state
+}
+
+export default productsReducer
