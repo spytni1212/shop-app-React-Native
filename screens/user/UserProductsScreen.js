57c75c2f9a3087f4b69c7be79a864a71d03af758
spytni1212 <avdeenkoartem1212@gmail.com>
2021-05-19 00:20:29 +0300

    feat: add handling user input

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -2,8 +2,8 @@ import React from 'react'
 import { FlatList, Platform, Button } from 'react-native'
 import { useSelector, useDispatch } from 'react-redux'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-
 import HeaderButton from '../../components/UI/HeaderButton'
+
 import ProductItem from '../../components/shop/ProductItem'
 import Colors from '../../constants/Colors'
 import { deleteProduct } from '../../store/actions/products'
