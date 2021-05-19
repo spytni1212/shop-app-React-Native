81194d9524373426ff49826cb2a69e891efa1a85
spytni1212 <avdeenkoartem1212@gmail.com>
2021-05-19 21:15:30 +0300

    feat: storing orders

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,11 +1,38 @@
 export const ADD_ORDER = 'ADD_ORDER'
 
 export const addOrder = (cartItems, totalAmount) => {
-	return {
-		type: ADD_ORDER,
-		orderData: {
-			items: cartItems,
-			amount: totalAmount,
-		},
+	return async dispatch => {
+		const date = new Date()
+
+		const response = await fetch(
+			'https://shop-app-a09a8-default-rtdb.firebaseio.com/orders/u1.json',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					cartItems,
+					totalAmount,
+					date: date,
+				}),
+			}
+		)
+
+		if (!response.ok) {
+			throw new Error('Something went wrong')
+		}
+
+		const respData = await response.json()
+
+		dispatch({
+			type: ADD_ORDER,
+			orderData: {
+				id: respData.name,
+				items: cartItems,
+				amount: totalAmount,
+				date: date,
+			},
+		})
 	}
 }
